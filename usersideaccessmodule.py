72903b809bb2ef7ed06c932442d3fad2805100d5
AJAYRAGAVAN.M.V <ajayragavanmv@gmail.com>
2020-12-01 08:22:08 +0530

    Update usersideaccessmodule.py
    Added necessary comments inside the usersideaccessmodule.py, to provide more clarity on the flow involved

--- usersideaccessmodule.py
+++ usersideaccessmodule.py
@@ -8,11 +8,12 @@ Key-value data store which supports basic CRD Operations
 #This is a input file on client side to access backend code to perform CRD Operations
 
 import fwbackendmodule as fw
+#fwbackendmodule which performs basic CRD Operations has been imported into this code as fw
 
 print("Welcome to Key-Value Datastore")
 print("Press 1 to Inititate single access datastore\nPress 2 to Inititate Multi access datastore\nPress any key to quit program")
 ip=int(input()) #to get user choice
-
+# If any key pressed other than 1 or 2, The program will end here
 if(ip == 1):
     fw.initiatefn()  #to call the single process fn in the other code
 elif(ip == 2):
